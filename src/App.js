85fa182edb4b92e6df889d2ee82bda1cf07a44bd
antinglimmy <antinglim@gmail.com>
2022-06-14 00:29:50 +0800

    14 June

--- src/App.js
+++ src/App.js
@@ -1,47 +1,201 @@
-import React from "react";
+// import logo from './logo.svg';
 import "./App.css";
-import { makeShuffledDeck } from "./utils.js";
+import React from "react";
+import bunImage from './images/595-5957396_hamburger-bun-png-hamburger-bun-transparent-background-png.png';
+import tomatoImage from './images/png-clipart-sliced-tomato-pizza-tomato-vegetarian-cuisine-vegetable-tomato-food-nightshade-family.png';
+import lettuceImage from './images/kisspng-romaine-lettuce-hamburger-salad-clip-art-lettuce-5abb7a7895a819.786040171522236024613.jpg';
 
-class App extends React.Component {
+// import React, { useState } from "react";
+// import img6 from '../../Assets/First.png'
+// import img7 from '../../Assets/Second.png'
+
+// const listOfBurgers = () => {
+//   hamburger:
+// }
+
+// const hamburger = ["Beef Patty", "Lettuce", "Tomato"]
+
+
+class IngredientForm extends React.Component {
   constructor(props) {
-    // Always call super with props in constructor to initialise parent class
     super(props);
     this.state = {
-      // Set default value of card deck to new shuffled deck
-      cardDeck: makeShuffledDeck(),
-      // currCards holds the cards from the current round
-      currCards: [],
+      selectedIngredient: "",
+      selectedIngredients: [],
+      bun: false,
+      hamburger: ["Beef Patty", "Lettuce", "Tomato"],
+      order: "hamburger"
     };
+
+    // this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  dealCards = () => {
-    this.setState((state) => ({
-      // Remove last 2 cards from cardDeck
-      cardDeck: state.cardDeck.slice(0, -2),
-      // Deal last 2 cards to currCards
-      currCards: state.cardDeck.slice(-2),
-    }));
-  };
+  checkIngredients = () => {
+    let score = 1
+    for (let ingredient of this.state.hamburger) 
+    {
+      if (this.state.selectedIngredients.includes(ingredient)) {
+      score +=1
+      }
+    } 
+    if (score === this.state.hamburger.length) {
+      return "You built the correct burger"
+    } else {
+      return "You built the wrong burger, try again!"
+    }
+    }
+  
+
+  handleClick = (event) => {
+      let selectedIngredient2 = event.target.innerText
+      this.setState((state) => ({
+         selectedIngredient: event.target.innerText,
+        selectedIngredients: [...state.selectedIngredients, selectedIngredient2],
+      // event.target.innerText === "Bun" && bun: true,
+      }));
+    }
+ 
+  // Location = () => {
+  // const [imageClicked, setImageClicked] = useState({
+  //   first: false,
+  //   second: false,
+  //   ground: false
+  // });
+    
+  //   onClickHandler = (order) => {
+  //   setImageClicked((prevState) => ({
+  //     ...prevState,
+  //     [order]: !prevState[order]
+  //   }));
+  // };
+    // const imgArr = [
+    //  {
+    //   id: 'bun',
+    //   image: bun
+    //  }, 
+    // ]
+
+    // const [open, setOpen] = useState(null);
+
+    // const [showImage, setShowImage] = useState({
+    //   showImageNow: true,
+    //   showImageId: null,
+    // })
+    // const {showImageNow, showImageId} = showImage;
+
+    // const OpenImage = (a) => {
+    //  setOpen(a.image);
+    //  setShowImage({
+    //    showImageNow: !showImageNow,
+    //    showImageId: a.id
+    //  });
+    // }
 
   render() {
-    const currCardElems = this.state.currCards.map(({ name, suit }) => (
-      // Give each list element a unique key
-      <div key={`${name}${suit}`}>
-        {name} of {suit}
-      </div>
-    ));
+   
+    return (<div>
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h3>High Card 🚀</h3>
-          {currCardElems}
-          <br />
-          <button onClick={this.dealCards}>Deal</button>
-        </header>
-      </div>
-    );
+ <header>
+        <h3>Burger Restaurant</h3>
+        <h3>Build me a {this.state.order}</h3>
+        <button onClick={this.handleClick}>Bun</button>
+{/* <button onClick={this.onClick= onClickHandler("ground")}>Bun</button> */}
+<button onClick={this.handleClick}>Tomato</button>
+<button onClick={this.handleClick}>Lettuce</button>
+</header>
+
+      {this.state.selectedIngredients.includes("Bun") === true && <img src={bunImage} alt="bun" className="Bun" />}
+      {this.state.selectedIngredients.includes("Tomato") === true && <img src={tomatoImage} alt="tomato" className="Tomato" />}
+      {this.state.selectedIngredients.includes("Lettuce") === true && <img src={lettuceImage} alt="lettuce" className="Lettuce" />}
+{/* <div>
+  {imageClicked.ground && <img src={bun} alt="bun" />}
+</div> */}
+
+
+<table>
+  <tbody>
+    <tr>
+      <th>Ingredient List Test</th>
+    </tr>
+      {this.state.selectedIngredients.map((d, index) => (
+    <tr>
+      <td key={index}>{d}</td>
+    </tr>))}
+  </tbody>
+      </table>
+      
+      {this.checkIngredients()}
+
+{/* {<img src={bun} alt={"Bun"}/>} */}
+
+ {/* {imgArr.map((a, i) => 
+           <div key={i}>
+             <button 
+              onClick={() => OpenImage(a)}
+              className="ground" 
+             >
+              {a.id}
+             </button>
+
+             <br />
+        
+             {showImageNow && showImageId === a.id ?
+               <img
+                src={open} 
+                alt={a.id} 
+               />
+             :null}
+           </div>
+         )} */}
+
+    </div>)};
+
   }
-}
 
-export default App;
+/* //         <header>
+//           <h3>Burger Restaurant</h3>
+//           <button onClick={this.handleClick}>Bun</button>
+//           <button onClick={this.handleClick}>Tomato</button>
+//           <button onClick={this.handleClick}>Lettuce</button>
+
+//           {/* {this.state.selectedIngredients} */
+//         </header>
+//      <table>
+//          <tbody>
+//     <tr>
+//       <th>Ingredient List</th>
+//     </tr>
+//  {this.state.selectedIngredients.map((d, index) =>  */}
+//       <tr>
+//         <td key={index}>{d}</td>
+//       </tr>
+//       )}
+//       </tbody>
+//       </table>
+
+//  {imgArr.map((a, i) => 
+//            <div key={i}>
+
+//              <button 
+//               onClick={() => OpenImage(a)}
+//               className="ground" 
+//              >
+//               {a.id}
+//              </button>
+
+//              <br />
+        
+//              {showImageNow && showImageId === a.id ?
+//                <img
+//                 src={open} 
+//                 alt={a.id} 
+//                />
+//              :null}
+//            </div>
+//          )}
+  
+    // )};
+  
+
+export default IngredientForm;
