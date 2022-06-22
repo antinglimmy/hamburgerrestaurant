83df343903a60b0d0c50551e795e8f40c147bd63
antinglimmy <antinglim@gmail.com>
2022-06-23 00:12:08 +0800

    23 June v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// import logo from './logo.svg';
 import "./App.css";
 import React from "react";
-import bunImage from './images/595-5957396_hamburger-bun-png-hamburger-bun-transparent-background-png.png';
-import tomatoImage from './images/png-clipart-sliced-tomato-pizza-tomato-vegetarian-cuisine-vegetable-tomato-food-nightshade-family.png';
-import lettuceImage from './images/kisspng-romaine-lettuce-hamburger-salad-clip-art-lettuce-5abb7a7895a819.786040171522236024613.jpg';
+// import bunImage from './images/595-5957396_hamburger-bun-png-hamburger-bun-transparent-background-png.png';
+// import tomatoImage from './images/png-clipart-sliced-tomato-pizza-tomato-vegetarian-cuisine-vegetable-tomato-food-nightshade-family.png';
+// import lettuceImage from './images/kisspng-romaine-lettuce-hamburger-salad-clip-art-lettuce-5abb7a7895a819.786040171522236024613.jpg';
 import { GenerateOrders } from "./utils.js";
+import {Burger} from "./burger.js";
 
 class IngredientForm extends React.Component {
   constructor(props) {
@@ -12,23 +12,14 @@ class IngredientForm extends React.Component {
     this.state = {
       selectedIngredient: "",
       selectedIngredients: [],
-      // bun: false,
-      // hamburger2: ["Bun", "Lettuce", "Tomato"],
-      // hamburger: {Bun: 1, Lettuce: 2, Tomato: 1 },
       order: GenerateOrders(),
-      // selectedIngredientsTally: {},
-      // selectedIngredientsObject: {"Bun": false, "Lettuce": false, "Tomato": false},
     };
-
-    // this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.tallyIngredients = this.tallyIngredients.bind(this);
   }
 
   tallyIngredients = () => {
     let selectedIngredientsTally = {};
-      console.log(this.state.selectedIngredients)
-    // let message = ""
     for (let i = 0; i < this.state.selectedIngredients.length; i++) {
       if (this.state.selectedIngredients[i] in selectedIngredientsTally) {
         selectedIngredientsTally[this.state.selectedIngredients[i]] += 1
@@ -37,18 +28,7 @@ class IngredientForm extends React.Component {
       }
     }
 
-    
-    // for (let i = 0; i < this.state.selectedIngredients.length; i++) {
-      // if (selectedIngredientsTally.Bun === this.state.hamburger.Bun) {
-      //   console.log("Buns are right")
-      // }
-      // if (selectedIngredientsTally.Lettuce === this.state.hamburger.Lettuce) {
-      //   console.log("Lettuce are right")
-      // }
-    
     let sortedSelectedIngredients = this.state.selectedIngredients.sort();
-    console.log(sortedSelectedIngredients)
-    console.log(this.state.order)
     for (let i = 0; i < sortedSelectedIngredients.length; i++) {
       if (!(sortedSelectedIngredients[i] === this.state.order[i])) {
         console.log("Something is missing")
@@ -63,8 +43,8 @@ class IngredientForm extends React.Component {
       this.setState((state) => ({
          selectedIngredient: event.target.innerText,
         selectedIngredients: [...state.selectedIngredients, selectedIngredient2],
-      // event.target.innerText === "Bun" && bun: true,
       }));
+    console.log(this.state)
   }
   
   render() {
@@ -75,18 +55,13 @@ class IngredientForm extends React.Component {
         <h1 className = "gameTitle">Burger Restaurant</h1>
         <h3>Build me a {this.state.order}</h3>       
         <button onClick={this.handleClick}>Bun</button>
-        {/* <button onClick={this.onClick= onClickHandler("ground")}>Bun</button> */}
         <button onClick={this.handleClick}>Tomato</button>
         <button onClick={this.handleClick}>Lettuce</button>
+        <button onClick={this.handleClick}>Cheese</button>
         
 </header>
 
-      {this.state.selectedIngredients.includes("Bun") === true && <img src={bunImage} alt="bun" className="Bun" />}
-      {this.state.selectedIngredients.includes("Tomato") === true && <img src={tomatoImage} alt="tomato" className="Tomato" />}
-      {this.state.selectedIngredients.includes("Lettuce") === true && <img src={lettuceImage} alt="lettuce" className="Lettuce" />}
-
-
-      <table>
+   {/* <table>
         <tbody>
           <tr>
       <th>Ingredient List</th>
@@ -96,7 +71,9 @@ class IngredientForm extends React.Component {
       <td>{d}</td>
         </tr>))}
         </tbody>
-      </table>
+      </table> */}
+
+      <Burger selectedIngredients={this.state.selectedIngredients} />
       
       <button onClick={this.tallyIngredients}>Tally Ingredients</button>
 
---
+++ src/burger.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Bun from './images/TopBun.png'
+import Tomato from './images/Tomato.png';
+import Lettuce from './images/Lettuce.png';
+import Cheese from './images/Cheese.png'
+import "./App.css";
+
+class CheeseClass extends React.Component {
+  render() {
+    return (
+      <img src={Cheese} alt="cheese" className="Cheese" />
+    )
+  }
+}
+
+class Burger extends React.Component {
+
+  render() {
+    const selectedIngredients = this.props.selectedIngredients;
+    return (
+      <div className = "burger">
+        <div> My burger</div>
+
+         {selectedIngredients.map(i => {
+           return <img src={i} alt={i} />;
+         })}
+
+
+        {/* {selectedIngredients.includes("Bun") === true && <img src={bunImage} alt="bun" className="Bun" />}
+        <br />
+        {selectedIngredients.includes("Tomato") === true && <img src={tomatoImage} alt="tomato" className="Tomato" />}
+        <br />
+        {selectedIngredients.includes("Lettuce") === true && <img src={lettuceImage} alt="lettuce" className="Lettuce" />}
+        <br /> */}
+        {selectedIngredients.includes("Cheese") === true && <CheeseClass />}
+      </div>
+    )
+  };
+}
+
+export { Burger, Cheese };
